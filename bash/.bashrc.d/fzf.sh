bc894d7690920cbf72979d75a96f41a2d2c89526
Prasanna Loganathar <pvl@prasannavl.com>
2019-10-31 22:43:59 +0530

    better fzf, fdfind config

--- bash/.bashrc.d/fzf.sh
+++ bash/.bashrc.d/fzf.sh
@@ -3,22 +3,21 @@
 # Let's exit if fasd isn't installed.
 if [[ ! $(command -v fzf) ]]; then return 0; fi
 
-
-if [[ $(command -v fd) ]]; then
-    export FZF_DEFAULT_COMMAND='fd --follow --hidden --exclude .git --color=always'
+if [[ $(command -v fdfind) ]]; then
+    export FZF_DEFAULT_COMMAND='fdfind --follow --hidden --exclude .git --color=always'
     export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
     export FZF_DEFAULT_OPTS="--ansi"
 fi
 
 # Pick from git install, or package manager install
-for x in $HOME/.fzf.bash /usr/share/fzf/shell/key-bindings.bash; do
+for x in $HOME/.fzf.bash /usr/share/fzf/shell/key-bindings.bash /usr/share/doc/fzf/examples/key-bindings.bash; do
     if [[ -f "$x" ]]; then
         source "$x"
         break
     fi
 done
 
-complete -F _fzf_path_completion -o default -o bashdefault mpv
-complete -F _fzf_dir_completion -o default -o bashdefault tree
-complete -F _fzf_path_completion -o default -o bashdefault exa
-complete -F _fzf_path_completion -o default -o bashdefault smplayer
+if [[ $(command -v mpv) ]]; then complete -F _fzf_path_completion -o default -o bashdefault mpv; fi
+if [[ $(command -v tree) ]]; then complete -F _fzf_dir_completion -o default -o bashdefault tree; fi
+if [[ $(command -v exa) ]]; then complete -F _fzf_path_completion -o default -o bashdefault exa; fi
+if [[ $(command -v smplayer) ]]; then complete -F _fzf_path_completion -o default -o bashdefault smplayer; fi
