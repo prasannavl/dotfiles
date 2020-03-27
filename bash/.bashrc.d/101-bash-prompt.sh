ecfc5b957e6859df2bdeba194de05d7a396ce044
Prasanna Loganathar <pvl@prasannavl.com>
2020-03-27 16:49:44 +0530

    Add git branch to bash prompt

--- bash/.bashrc.d/101-bash-prompt.sh
+++ bash/.bashrc.d/101-bash-prompt.sh
@@ -1,38 +1,92 @@
-r="\[$(tput sgr0)\]"
-b="\[$(tput bold)\]"
+# Bash Colors
+# 
+# Text Format           Foreground (text) color         Background color
+# 0: normal text        30: Black                       40: Black
+# 1: bold	            31: Red	                        41: Red
+# 4: Underlined text    32: Green                       42: Green
+#                       33: Yellow                      43: Yellow
+#                       34: Blue                        44: Blue
+#                       35: Purple                      45: Purple
+#                       36: Cyan                        46: Cyan
+#                       37: White                       47: White
+# 
+# ESC sequence: \033 or \e
+# Color sequence: \[ESC[fg;format;bgm\] or \[$(tput setaf/ab color0-7)\] 
+# tput => terminfo, ncurses
 
-fwhite="\[\033[38;5;15m\]"
-fblack="\[\033[38;5;0m\]"
+function setup_ps1() {
+    # reset
+    local r="\[$(tput sgr0)\]"
+    local b="\[$(tput bold)\]"
 
-fgray="\[\033[38;5;7m\]"
-fgreen="\[\033[38;5;2m\]"
-fblue="\[\033[38;5;33m\]"
-fred="\[\033[38;5;196m\]"
+    local fwhite="\[\033[38;5;15m\]"
+    local fblack="\[\033[38;5;0m\]"
+ 
+    local fgray="\[\033[38;5;7m\]"
+    local fgreen="\[\033[38;5;2m\]"
+    local fblue="\[\033[38;5;33m\]"
+    local fred="\[\033[38;5;196m\]"
+    local fpink="\[\033[38;5;13m\]"
 
-# date="\D{%Y-%m-%d}"
-error="${b}${fred}\$(e="\$?";[[ "\$e" == "0" ]] || echo [exit: "\$e"])"
+    local error="${b}${fred}\$(e="\$?";[[ "\$e" == "0" ]] || printf \"[exit: "\$e"]\n\n\")"
 
-export PS1="${fgray}[\t|${b}${fgreen}\u${r}${fgreen}@\h\
-${r}${fgray}:${r}${fblue}\w${r}${fgray}] \
-${error}${r}\\n\$ "
+    local ps1_main_line="${error}${r}${fgray}[\t|${b}${fgreen}\u${r}${fgreen}@\h\
+${r}${fgray}:${r}${fblue}\w${r}${fgray}]"
 
-unset r b fwhite fblack fgray fgreen fblue fred error
+    local ps1_exit_line="${r}\\n\$ "
 
-# bdir="\[\033[48;5;237m\]"
-# fdir="\[\033[38;5;252m\]"
+    if [[ $(command -v git) ]]; then
+        local ps1_main_line="${ps1_main_line} ${fpink}\`_parse_git_branch\`"
+    fi
 
-# ftime="\[\033[38;5;250m\]"
-# btime="\[\033[48;5;240m\]"
+    export PS1="${ps1_main_line}${ps1_exit_line}"
+}
 
-# berr="\[\033[48;5;1m\]"
-# ferr="\[\033[38;5;255m\]"
+# get current branch in git repo
+function _parse_git_branch() {
+	BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+	if [ ! "${BRANCH}" == "" ]
+	then
+		STAT=`_parse_git_dirty`
+		echo "(${BRANCH}${STAT})"
+	else
+		echo ""
+	fi
+}
 
-# fdark="\[\033[38;5;241m\]"
+# get current status of git repo
+function _parse_git_dirty {
+	status=`git status 2>&1 | tee`
+	dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
+	untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
+	ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
+	newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
+	renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
+	deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+	bits=''
+	if [ "${renamed}" == "0" ]; then
+		bits=">${bits}"
+	fi
+	if [ "${ahead}" == "0" ]; then
+		bits="*${bits}"
+	fi
+	if [ "${newfile}" == "0" ]; then
+		bits="+${bits}"
+	fi
+	if [ "${untracked}" == "0" ]; then
+		bits="?${bits}"
+	fi
+	if [ "${deleted}" == "0" ]; then
+		bits="x${bits}"
+	fi
+	if [ "${dirty}" == "0" ]; then
+		bits="!${bits}"
+	fi
+	if [ ! "${bits}" == "" ]; then
+		echo " ${bits}"
+	else
+		echo ""
+	fi
+}
 
-# error="${ferr}${berr}\$(e="\$?";[[ "\$e" == "0" ]] || echo \ \ exit: "\$e"\ \ )"
-
-# export PS1="${ftime}${btime}  \t  ${r}${fdir}${bdir}  \u@\h  \
-# ${fdark}│${b}${fgreen}  \w  ${r}\
-# ${error}${r}\\n\$ "
-
-# unset bdir fdir ftime btime berr ferr fdark error
+setup_ps1
