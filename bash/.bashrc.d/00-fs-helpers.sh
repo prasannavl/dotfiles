814e51b8cc8d9b0c320f79a6596ebefe8732dab0
Prasanna Loganathar <pvl@prasannavl.com>
2025-08-03 16:56:57 +0800

    Add mkcd mvx helpers

--- bash/.bashrc.d/00-fs-helpers.sh
+++ bash/.bashrc.d/00-fs-helpers.sh
@@ -4,3 +4,23 @@ __source_file_if_exists() {
 		. "$file"
 	fi
 }
+
+# mkdir -p and cd
+mkcd() {
+    mkdir -p "$@" && cd "$@"
+}
+
+# swap file name with an auto tmp file
+mvx() {
+    (
+        set -Eeuo pipefail
+        local suffix
+        suffix="$(set +o pipefail && cat /dev/urandom | head -c12 | md5sum)"
+        echo "mv: $1 -> $1.${suffix}"
+        mv "$1" "$1.${suffix}"
+        echo "mv: $2 -> $1"
+        mv "$2" "$1"
+        echo "mv $1.${suffix} -> $2"
+        mv "$1.${suffix}" "$2"
+    )
+}
