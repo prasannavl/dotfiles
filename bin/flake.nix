e4914808b0605a2317f781aa9e1efc19ac476bec
Prasanna Loganathar <pvl@prasannavl.com>
2026-02-15 10:53:15 +0800

    Add formatter

--- bin/flake.nix
+++ bin/flake.nix
@@ -3,129 +3,125 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          commonDeps = with pkgs; [
-            bash
-            coreutils
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "x86_64-linux"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    formatter = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in
+        pkgs.alejandra
+    );
+
+    packages = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        commonDeps = with pkgs; [
+          bash
+          coreutils
+        ];
+
+        scriptDeps = {
+          "amdgpu-reset.sh" = with pkgs; [sudo];
+          "app-scope.sh" = with pkgs; [systemd];
+          "battery-info.sh" = with pkgs; [upower];
+          "battery-threshold.sh" = with pkgs; [
+            gnugrep
+            sudo
           ];
+          "brightness-ddcci.sh" = with pkgs; [findutils];
+          "brightness.sh" = with pkgs; [sudo];
+          "colortest.sh" = [];
+          "diff-oneside.sh" = with pkgs; [diffutils];
+          "git-sparse-clone.sh" = with pkgs; [git];
+          "ip-external.sh" = with pkgs; [dnsutils];
+          "mutter-reset-displays.sh" = with pkgs; [systemd];
+          "niri-session.sh" = with pkgs; [
+            gawk
+            gnome-keyring
+            niri
+            procps
+            swayidle
+            swaylock
+            systemd
+          ];
+          "nvexec.sh" = [];
+          "pkg.mod.sh" = [];
+          "swap-flush.sh" = with pkgs; [
+            sudo
+            util-linux
+          ];
+          "sway-session.sh" = with pkgs; [
+            gawk
+            gnome-keyring
+            procps
+            sway
+            swayidle
+            swaylock
+            systemd
+          ];
+          "sys-update.sh" = with pkgs; [
+            curl
+            dmidecode
+            flatpak
+            fwupd
+            gnugrep
+            htmlq
+            nix
+            "nvidia-settings"
+            pup
+            sudo
+          ];
+          "tmux-ssh.sh" = with pkgs; [openssh tmux];
+          "to-lower.sh" = [];
+          "vnc-start-scraping.sh" = with pkgs; [tigervnc];
+          "world-clock.sh" = [];
+        };
 
-          scriptDeps = {
-            "amdgpu-reset.sh" = with pkgs; [ sudo ];
-            "app-scope.sh" = with pkgs; [ systemd ];
-            "battery-info.sh" = with pkgs; [ upower ];
-            "battery-threshold.sh" = with pkgs; [
-              gnugrep
-              sudo
-            ];
-            "brightness-ddcci.sh" = with pkgs; [ findutils ];
-            "brightness.sh" = with pkgs; [ sudo ];
-            "colortest.sh" = [ ];
-            "diff-oneside.sh" = with pkgs; [ diffutils ];
-            "git-sparse-clone.sh" = with pkgs; [ git ];
-            "ip-external.sh" = with pkgs; [ dnsutils ];
-            "mutter-reset-displays.sh" = with pkgs; [ systemd ];
-            "niri-session.sh" = with pkgs; [
-              gawk
-              gnome-keyring
-              niri
-              procps
-              swayidle
-              swaylock
-              systemd
-            ];
-            "nvexec.sh" = [ ];
-            "pkg.mod.sh" = [ ];
-            "swap-flush.sh" = with pkgs; [
-              sudo
-              util-linux
-            ];
-            "sway-session.sh" = with pkgs; [
-              gawk
-              gnome-keyring
-              procps
-              sway
-              swayidle
-              swaylock
-              systemd
-            ];
-            "sys-update.sh" = with pkgs; [
-              curl
-              dmidecode
-              flatpak
-              fwupd
-              gnugrep
-              htmlq
-              nix
-              "nvidia-settings"
-              pup
-              sudo
-            ];
-            "tmux-ssh.sh" = with pkgs; [
-              openssh
-              tmux
-            ];
-            "to-lower.sh" = [ ];
-            "vnc-start-scraping.sh" = with pkgs; [ tigervnc ];
-            "world-clock.sh" = [ ];
+        wrapCommands = pkgs.lib.concatStringsSep "\n" (
+          pkgs.lib.mapAttrsToList (
+            name: deps: ''
+              wrapProgram "$out/bin/${name}" \
+                --prefix PATH : ${pkgs.lib.makeBinPath (commonDeps ++ deps)}
+            ''
+          )
+          scriptDeps
+        );
+      in {
+        default = pkgs.stdenvNoCC.mkDerivation {
+          pname = "dotfiles-bin";
+          version = "0.1.0";
+          src = ./.;
+
+          nativeBuildInputs = [pkgs.makeWrapper];
+
+          dontConfigure = true;
+          dontBuild = true;
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p "$out/bin"
+            cp -a "$src"/*.sh "$out/bin/"
+            chmod +x "$out/bin"/*.sh
+
+            ${wrapCommands}
+
+            runHook postInstall
+          '';
+
+          meta = {
+            description = "Portable wrappers for personal scripts";
+            platforms = pkgs.lib.platforms.linux;
           };
-
-          scriptPaths = pkgs.lib.mapAttrs (_: deps: pkgs.lib.makeBinPath (commonDeps ++ deps)) scriptDeps;
-          defaultPath = pkgs.lib.makeBinPath commonDeps;
-          wrapperCase =
-            pkgs.lib.concatStringsSep "\n"
-              (pkgs.lib.mapAttrsToList (name: path: ''
-                "${name}") depPath='${path}' ;;
-              '') scriptPaths);
-        in
-        {
-          default = pkgs.stdenvNoCC.mkDerivation {
-            pname = "dotfiles-bin";
-            version = "0.1.0";
-            src = ./.;
-
-            nativeBuildInputs = [ pkgs.makeWrapper ];
-
-            dontConfigure = true;
-            dontBuild = true;
-
-            installPhase = ''
-              runHook preInstall
-
-              mkdir -p "$out/bin"
-              cp -a "$src"/*.sh "$out/bin/"
-              chmod +x "$out/bin"/*.sh
-
-              for script in "$out"/bin/*.sh; do
-                name="$(basename "$script")"
-                depPath='${defaultPath}'
-                case "$name" in
-                  ${wrapperCase}
-                esac
-                wrapProgram "$script" --prefix PATH : "$depPath"
-              done
-
-              runHook postInstall
-            '';
-
-            meta = {
-              description = "Portable wrappers for personal scripts";
-              platforms = pkgs.lib.platforms.linux;
-            };
-          };
-        }
-      );
-    };
+        };
+      }
+    );
+  };
 }
